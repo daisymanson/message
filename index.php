627261122647adfd835f2978ef292242e24ce71d
Daisy <daisy_wang_2@yahoo.com.tw>
2018-08-22 14:02:43 +0800

    add table to put the data

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>留言板</title>
+  <style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 5px;
+        text-align: left;
+    }
+  </style>
 </head>
 </html>
 
@@ -12,28 +22,41 @@ $db = new DB;
 
 $result = select_list($db,$arr_input);
 
+echo "留言板";
+echo "<table>";
+echo "<tr>";
+echo "<th>No</th>";
+echo "<th>ID</th>";
+echo "<th>Content</th>";
+echo "<th>Date</th>";
+echo "<th colspan='2'>btn</th>";
+echo "</tr>";
+
 if(is_array($result) == true)
 {
   foreach ($result as $key => $value1)
   {
-    echo ($key + 1).'<br />';
-    echo $value1['guest_id'].'<br />';
-    echo $value1['content'].'<br />';
-    echo $value1['date'].'<br />';
-    echo '<hr />';
+    echo "<tr>";
+    echo "<td>".($key + 1)."</td>";
+    echo "<td>".$value1['guest_id']."</td> ";
+    echo "<td>".$value1['content']."</td> ";
+    echo "<td>".$value1['date']."</td> ";
+    echo "<td>"."update"."</td>";
+    echo "<td>"."delete"."</td>";
+    echo "</tr>";
+    echo "</table>";
   }
 }
 
 unset($arr_input);
 unset($sql_input);
 
-
 function select_list($db, $arr_input)
 {
-    $result = $db->table('message')
-                 ->select('guest_id, content, date')
-                 ->where('is_enable = 0', $arr_input)
-                 ->get();
-    return $result;
+  $result = $db->table('message')
+               ->select('guest_id, content, date')
+               ->where('is_enable = 0', $arr_input)
+               ->get();
+  return $result;
 }
 ?>
