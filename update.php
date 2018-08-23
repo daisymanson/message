1487e66b16ff09869f3698cbec55f8a60a44aec6
Daisy <daisy_wang_2@yahoo.com.tw>
2018-08-23 16:16:23 +0800

    modify the code

--- update.php
+++ update.php
@@ -1,36 +1,43 @@
+<?php
+require_once('class_db_maria.php');
+$db = new DB;
+
+$arr_input['id'] = $_GET['id'];
+$arr_input['content'] = $_POST['content'];
+
+$sql_input['id'] = $arr_input['id'];
+$res = get_list($db, $sql_input);
+unset($sql_input);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <title>留言板</title>
 </head>
-<form action="" method="POST">
-  <?php echo $_GET['id']; ?>
-  <br>
-  Content
-  <input type="text" name="content"></input>
-  <input type="submit" value="Submit"></input>
-  <input type ="button" onclick="history.back()" value="Cancel"></input>
+<?php
+if(is_array($res) == ture)
+{
+  ?>
+  <form action="check_update.php" method="POST">
+    <br>
+    Content
+    <input type="text" name="content" value="<?php echo $res[0]['content'];?>"></input>
+    <input type="hidden" name="id" value="<?php echo $res[0]['id'];?>"></input>
+    <input type="submit" value="Submit"></input>
+    <input type ="button" onclick="history.back()" value="Cancel"></input>
+<?php
+}
+else
+{
+  echo 'NoData';
+}
+?>
 </form>
 </html>
 
 <?php
-require_once('class_db_maria.php');
-$db = new DB;
-
-$arr_input['id'] = $_GET['id'];
-
-get_list($db, $arr_input);
-
-$array_input['content'] = $_POST['content'];
-$array_defult['id'] = $arr_input['id'];
-
-mod_list($db, $array_input, $array_defult);
-
-unset($arr_input);
-unset($array_defult);
-unset($array_input);
-
 function get_list($db, $arr_input)
 {
 	$result = $db->table("message")
@@ -38,14 +45,5 @@ function get_list($db, $arr_input)
                ->where('is_enable = 0 AND id = ?', $arr_input)
 				       ->get();
 	return $result;
-}
-
-function mod_list($db,$array_input, $array_defult)
-{
-	$res = $db->table('message')
-					  ->update($array_input)
-					  ->where('id = ? ', $array_defult)
-					  ->get();
-	return $res;
 }
 ?>
