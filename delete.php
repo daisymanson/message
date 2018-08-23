d3b0f241c851cfd9736f3c72604d176ea42cd684
Daisy <daisy_wang_2@yahoo.com.tw>
2018-08-23 14:43:47 +0800

    add delete

--- delete.php
+++ delete.php
@@ -1,7 +1,37 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-</head>
-</html>
-<?php  ?>
+<?php
+require_once('class_db_maria.php');
+$db = new DB;
+
+$arr_def['id'] = $_GET['id'];
+
+$arr_inpt['is_enable'] = 1;
+
+$result = checke_data($db, $arr_def);
+
+if(is_array($result) == ture)
+{
+  delete_list($db, $arr_inpt, $arr_def);
+}
+else
+{
+  var_dump($result);
+}
+
+function delete_list($db, $arr_inpt, $arr_def)
+{
+	$res = $db->table('message')
+					  ->update($arr_inpt)
+					  ->where('id = ? ', $arr_def)
+					  ->get();
+	return $res;
+}
+
+function checke_data($db, $arr_def)
+{
+  $result = $db->table("message")
+               ->select('*')
+               ->where('id = ?', $arr_def)
+               ->get();
+  return $result;
+}
+?>
